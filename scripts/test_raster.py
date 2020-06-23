d83f0f0e95b1a330c8d93cc14f8aa47f53dcd110
Micah Johnson <micah.johnson150@gmail.com>
2020-06-23 15:33:05 -0600

    Verified raster was uploaded to the db!

--- scripts/test_raster.py
+++ scripts/test_raster.py
@@ -1,6 +1,8 @@
 from snowxsql.data import RasterData
 from snowxsql.db import get_db
 from sqlalchemy import func
+from rasterio.io import MemoryFile
+from rasterio import plot
 
 
 # Connect to the database we made.
@@ -9,8 +11,15 @@ db_name = 'postgresql+psycopg2:///snowex'
 engine, metadata, session = get_db(db_name)
 
 # Query the datbase looking at BulkLayerData, filter on comments containing graupel (case insensitive)
-records = session.query(RasterData, func.ST_AsGDALRaster(RasterData.raster,'GTiff')).all()
+r = session.query(func.ST_AsGDALRaster(RasterData.raster,'GTiff')).all()[0]
 
+#print(help(r))
+bdata = bytes(r[0])
 
-for r in records:
-    print(r)
+with MemoryFile() as tmpfile:
+    tmpfile.write(bdata)
+
+    with tmpfile.open() as dataset:
+        plot.show(dataset, cmap='viridis')
+
+session.close()
