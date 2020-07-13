64f6168e60fe053b8ea73bbae55d9ebdf3443262
Micah Johnson <micah.johnson150@gmail.com>
2020-07-13 14:45:28 -0600

    working on raster examples, turns out plotting all the rasters is too much memory

--- scripts/plot_raster.py
+++ scripts/plot_raster.py
@@ -39,7 +39,7 @@ circle = to_shape(buffered_pit)
 print('Grabbing rasters that overlap on the {}m radius centered on {}'.format(buffer_dist, pit))
 rasters = session.query(func.ST_AsTiff(ST_RasterUnion(RasterData.raster))).filter(gfunc.ST_Intersects(RasterData.raster, buffered_pit)).all()
 
-# Create a 
+# Create a
 nearby_pits = session.query(LayerData.geometry).filter(gfunc.ST_Within(LayerData.geometry, buffered_pit))
 nearby_pits = query_to_geopandas(nearby_pits, engine)
 
---
+++ scripts/plot_survey_overview.py
@@ -0,0 +1,52 @@
+from snowxsql.db import get_db
+from snowxsql.conversions import raster_to_rasterio
+from snowxsql.data import RasterData, LayerData
+from geoalchemy2.types import Raster
+from rasterio.plot import show
+from sqlalchemy.sql import func
+import geoalchemy2.functions as gfunc
+from geoalchemy2.shape import to_shape
+from rasterio import MemoryFile
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+from snowxsql.db import get_db
+from snowxsql.data import RasterData, LayerData
+from rasterio.plot import show
+from sqlalchemy.sql import func
+import geoalchemy2.functions as gfunc
+from geoalchemy2.shape import to_shape
+from rasterio import MemoryFile
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from snowxsql.conversions import raster_to_rasterio
+from snowxsql.functions import ST_RasterUnion
+from snowxsql.conversions import points_to_geopandas, query_to_geopandas
+
+# TURNS OUT THIS DRAWS TOO MUCH MEMORY FOR POSTGRES :(
+
+# Connect to the database we made.
+db_name = 'postgresql+psycopg2:///snowex'
+engine, metadata, session = get_db(db_name)
+datasets = []
+
+# Grab all rasters touching the circle, form a single raster, convert to tiff
+rasters = session.query(func.ST_AsTiff(func.ST_Rescale(ST_RasterUnion(RasterData.raster), 5), 'DEFLATE9')).all()
+
+
+dataset = raster_to_rasterio(session, rasters)[0]
+
+fig,ax = plt.subplots()
+
+img = show(dataset.read(1), ax=ax, transform=dataset.transform, cmap='terrain')
+show(dataset.read(1), contour=True, colors='k', ax=ax, transform=dataset.transform)
+
+
+ax.set_xlabel('Easting [m]')
+ax.set_ylabel('Northing [m]')
+plt.suptitle('     Pit {} w/ {}m Radius Circle on QSI DEM'.format(site_id, buffer_dist))
+plt.tight_layout()
+ax.legend()
+plt.show()
+dataset.close()
+session.close()
