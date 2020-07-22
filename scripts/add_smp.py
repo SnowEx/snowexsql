b258fa7c4c07251bd1f794469ae7f89b4742727c
Micah Johnson <micah.johnson150@gmail.com>
2020-07-22 13:09:34 -0600

    Added placeholders for SMP and SSA upload scripts

---
+++ scripts/add_smp.py
@@ -0,0 +1,7 @@
+'''
+Download your data from https://drive.google.com/u/0/uc?export=download&confirm=QqLp&id=1lcST1PrYqhvCD0BvZMAUdgGYlVDSq5ay
+
+Unzip in your downloads
+
+run this script
+'''
---
+++ scripts/add_ssa.py
@@ -0,0 +1,7 @@
+'''
+Added ssa measurements to the database
+
+Download from https://osu.app.box.com/s/7yq08y1mqpl9evgz6rfw8hu771228ryn
+
+Unzip to ~/Downloads
+'''
