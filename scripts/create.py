630458c91e5fcc261b98e56f255b67a89ae08ebd
Micah Johnson <micah.johnson150@gmail.com>
2020-07-31 08:30:32 -0600

    Added a test for clean str

--- scripts/create.py
+++ scripts/create.py
@@ -3,11 +3,20 @@ Script used to create the database and tables for the first time
 '''
 from snowxsql.create_db import initialize
 from snowxsql.db import get_db
+from snowxsql.utilities import get_logger
 
+log = get_logger('Create DB')
 db_name = 'postgresql+psycopg2:///snowex'
 
 engine, metadata, session = get_db(db_name)
 
-input('WARNGING! You are about to overwrite the entire database! Press any key to proceed:\n')
-initialize(engine)
+a = input('\nWARNING! You are about to overwrite the entire database! Press Y to continue, press any other key to abort:\n')
+if a.lower() == 'y':
+    log.warning('Clearing Database...')
+    initialize(engine)
+    log.info('Complete!')
+
+else:
+    log.warning('Aborted. Database has not been modified.')
+
 session.close()
--- tests/test_string_management.py
+++ tests/test_string_management.py
@@ -2,7 +2,7 @@ from snowxsql.string_management import *
 import pytest
 
 
-@pytest.mark.filterwarnings("ignore:Assuming")
+@pytest.mark.filterwarnings('ignore:Assuming')
 def test_cardinal_to_degrees():
     '''
     Test if we can convert cardinal directions correctly
@@ -27,3 +27,14 @@ def test_cardinal_to_degrees():
     # Test composite directions
     with pytest.raises(ValueError):
         d = convert_cardinal_to_degree('')
+
+def test_clean_str():
+    '''
+    Test whether we can clean out the column header from csv and standardize them
+    '''
+    # Test whether we replace spaces with underscores
+    test = ['SMP instrument #', 'Dielectric Constant A', 'Specific surface area (m^2/kg)']
+    result = ['smp_instrument_#', 'dielectric_constant_a', 'specific_surface_area']
+
+    for i,t in enumerate(test):
+        assert clean_str(t)== result[i]
