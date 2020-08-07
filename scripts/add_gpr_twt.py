9778f6bed04ebc3124b9c97fd637acdbe8f82cc7
Micah Johnson <micah.johnson150@gmail.com>
2020-08-07 13:43:26 -0600

    Added the real path from tate fro the full gpr data, 1264905 lines!

--- scripts/add_gpr_twt.py
+++ scripts/add_gpr_twt.py
@@ -1,34 +1,46 @@
 '''
 Read in the SnowEx GPR data of two way travel time and submit them to the db
+
+Download data from:
+https://drive.google.com/file/d/1QS7N0h7ixkiZcLv0xpme5A8rQTLvGMUJ/view
+
+Extract to Downloads
+
+Usage:
+    python add_gpr_twt.py
+
 '''
 
 
 import pandas as pd
-from os.path import join, abspath
+from os.path import join, abspath, expanduser
 import time
 
 from snowxsql.upload import *
 from snowxsql.db import get_db
 
 def main():
+
+    # Directory containing mulitlpe folders of
+
     # Site name
     start = time.time()
     site_name = 'Grand Mesa'
     timezone = 'MST'
 
-    # Read in the Grand Mesa Snow Depths Data
-    fname = abspath(join('..', '..', 'SnowEx2020_SQLdata',
-                               'GPR',
-                               'BSU_pE_GPR_01282020_00_TWT.csv'))
+    # Add the entire gpr dataset
+    fname = join('~','Downloads','NSIDC_GPR_Package','SNEX20_BSU_GPR_TT',
+                                 'BSU_pE_GPR_01282020_01292020_02042020_TWT',
+                                 'BSU_pE_GPR_01282020_01292020_02042020_TWT.csv')
 
     # Start the Database
     db_name = 'postgresql+psycopg2:///snowex'
     engine, metadata, session = get_db(db_name)
 
-    csv = PointDataCSV(fname, units='ns', site_name=site_name,
+    csv = PointDataCSV(abspath(expanduser(fname)), units='ns', site_name=site_name,
                                           timezone=timezone,
                                           epsg=26912,
-                                          surveyors='Tate Meehan', 
+                                          surveyors='Tate Meehan',
                                           instrument='Pulse Ekko Pro')
     csv.submit(session)
 
