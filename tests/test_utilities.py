4f5d1a392ee99c6b9fdd78dc94614f5182c0c6e7
Micah Johnson <micah.johnson150@gmail.com>
2020-08-19 10:58:32 -0600

    Added a test for reading individual lines from files

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,14 @@
 from snowxsql.utilities import *
 import numpy as np
+from os.path import dirname, join
+
+def test_read_nline():
+    '''
+    Test we can read a specific numbe of lines from a file
+    '''
+    f = join(dirname(__file__), 'data', 'density.csv')
+    line = read_n_lines(f, 1)
+    assert line[0] == '# Location,Grand Mesa\n'
 
 def test_find_files():
     '''
