8a1557b90fd532a540846d7b5201758da0f17166
Micah Johnson <micah.johnson150@gmail.com>
2020-09-24 08:49:08 -0600

    Added ASO Snow off

--- scripts/add_aso_snowoff.py
+++ scripts/add_aso_snowoff.py
@@ -2,10 +2,12 @@
 Upload the SnowEx Raster for snow off over Grand Mesa 2016-2017 flown by
 ASO
 '''
-from os.path import isdir, dirname, abspath, expanduser
+from os.path import isdir, dirname, abspath, expanduser, basename, join
+from os import mkdir
+from shutil import rmtree
 from datetime import date
 from snowxsql.batch import UploadRasterBatch
-
+from snowxsql.projection import reproject_raster_by_epsg
 
 def main():
     '''
@@ -28,8 +30,28 @@ def main():
               'tiled':True
               }
 
+    # 2B. Convert image from UTM13 to 12
+    out_dir = join(dirname(filenames[0]), 'utm_12')
+
+    if isdir(out_dir):
+        rmtree(out_dir)
+
+    mkdir(out_dir)
+
+    final = []
+
+    for r in filenames:
+        # Construct a new filename
+        out = join(out_dir, basename(r))
+
+        # Reproject the raster and output to the new location
+        reproject_raster_by_epsg(r, out, 26912)
+
+        # Keep the new file name
+        final.append(out)
+
     # 3, Pass them to you batch uploader you need
-    u = UploadRasterBatch(filenames, **kwargs)
+    u = UploadRasterBatch(final, **kwargs)
 
     # 4. Push to the database and collect the errors from push function
     errors = u.push()
