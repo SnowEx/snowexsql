40bbc5a39de4acaed7e52920783c48bde34de108
micah johnson <micah.johnson150@gmail.com>
2021-05-30 08:33:24 -0600

    Added some arg parse to the create.py to allow the install to assign users to the db and grant access in the install _ubuntu script. Working on #31

--- scripts/install/install_ubuntu.sh
+++ scripts/install/install_ubuntu.sh
@@ -59,20 +59,23 @@ python3 modify_conf.py
 # Restart the postgres service to update the changes in the conf file
 sudo service postgresql restart
 
+# Create roles before db
+sudo -u postgres psql -c "CREATE USER $DB_BUILDER WITH PASSWORD 'db_builder';"
+sudo -u postgres psql -c "CREATE USER $READ_ONLY WITH PASSWORD 'hackweek';"
+
 # Loop over the databases and create them and install extensions
 for DB in $DATABASES
   do
     echo Creating $DB database
-    # Create it
-    createdb $DB
+    # Create the dbs that the database build owns
+    createdb -O $DB_BUILDER $DB
 
     # Install postgis
     psql $DB -c 'CREATE EXTENSION postgis; CREATE EXTENSION postgis_raster;'
-
+    python3 ../upload/create.py --overwrite --db $DB --credentials ../upload/credentials.json
   done
 
-# Establish ubuntu as our uploading user
-psql snowex -c "CREATE USER $DB_BUILDER WITH PASSWORD 'db_builder';"
+# Establish builder as our uploading user
 psql snowex -c "GRANT CONNECT ON DATABASE snowex TO $DB_BUILDER;"
 psql snowex -c "GRANT CONNECT ON DATABASE test TO $DB_BUILDER;"
 psql snowex -c "GRANT USAGE ON SCHEMA public TO $DB_BUILDER;"
@@ -80,9 +83,11 @@ psql snowex -c "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA pub
 
 # Establish snow as a readonly user based on
 # https://medium.com/@swaplord/grant-read-only-access-to-postgresql-a-database-for-a-user-35c57897dd0b
-psql snowex -c "CREATE USER $READ_ONLY WITH PASSWORD 'hackweek';"
 psql snowex -c "GRANT CONNECT ON DATABASE snowex TO $READ_ONLY;"
 psql snowex -c "GRANT USAGE ON SCHEMA public TO $READ_ONLY;"
+
+# Create the tables
+
 #psql snowex -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO $READ_ONLY;"
 psql snowex -c "GRANT SELECT ON sites TO $READ_ONLY;"
 psql snowex -c "GRANT SELECT ON points TO $READ_ONLY;"
