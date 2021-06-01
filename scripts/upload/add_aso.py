c920cac92500898144b3a998b34f5208290b78fe
micah johnson <micah.johnson150@gmail.com>
2021-05-31 22:37:37 -0600

    Working upload script for adding ASO grand mesa data

--- scripts/upload/add_aso.py
+++ scripts/upload/add_aso.py
@@ -1,83 +1,60 @@
-'''
-Upload the SnowEx Raster for snow off over Grand Mesa 2016-2017 flown by
-ASO
+"""
+Upload the SnowEx ASO product Grand Mesa, East River, and Reynolds Creek from 2020
 
-Spatial Reference:
-    * EPSG: 32613 (Needs to be reprojected)
+# To run with all the scripts
+python run.py
 
-We found that to match this with the other datasets:
-
-gdalwarp -r bilinear -t_srs 26912 USCOGM20160926f1a1__lowest_vf_snowEX_extent.tif utm_12/USCOGM20160926f1a1__lowest_vf_snowEX_extent.tif
-
-Then you need to shift the vertical datum:
-We had to use the the tool: dem_geoid  from https://ti.arc.nasa.gov/tech/asr/groups/intelligent-robotics/ngt/stereo/
-
-dem_geoid -o test utm_12/USCOGM20160926f1a1__lowest_vf_snowEX_extent.tif
-mv utm12/test-adj.tif utm_12/USCOGM20160926f1a1__lowest_vf_snowEX_extent.tif
-
-Usage:
-    # To run with all the scripts
-    python run.py
-
-    # To run individually
-    python add_aso_snowoff.py
-'''
-
-
+# To run individually
+python add_aso.py
+"""
 from datetime import date
-from os import mkdir
-from os.path import abspath, basename, dirname, expanduser, isdir, join
-from shutil import rmtree
-from subprocess import check_output
+from os.path import join
 
-from add_QSI import reproject
 from snowexsql.batch import UploadRasterBatch
-from snowexsql.projection import reproject_raster_by_epsg
-from snowexsql.utilities import get_logger
 
 
 def main():
-    '''
+    """
     Uploader script for ASO Snow off data
-    '''
-    epsg = 26912
+    """
 
-    # 1. Define the files, in this case only one
-    filenames = [
-        '~/Downloads/ASO2016-17-20200918T212934Z-001/ASO2016-17/USCOGM20160926f1a1__lowest_vf_snowEX_extent.tif']
-
-    # 1B. Expand paths to full absolute paths
-    filenames = [abspath(expanduser(f)) for f in filenames]
-
-    # 2. Assign any contant metadata and pass it as keyword arguments to the
-    # uploader
+    # Typical kwargs
     kwargs = {'instrument': 'lidar',
-              'surveyors': 'ASO',
-              'date': date(2016, 9, 26),
-              'type': 'DEM',
-              'units': 'meters',
-              'description': 'Snow off DEM flown by ASO for SNOWEX 2017',
+              'surveyors': 'ASO Inc.',
+              'description': '50m product',
               'tiled': True,
-              'epsg': epsg,
+              'epsg': 26912,
               'no_data': -9999
               }
+    # Build a list of uploaders and then execute them
+    uploaders = []
 
-    # # 2B. Convert image from UTM13 to 12
-    out_dir = join(dirname(filenames[0]), 'utm_12')
+    # Directory of ASO products reprojected
+    reprojected = '../download/data/aso/reprojected'
 
-    log = get_logger('ASO Uploader')
+    ########################################### Grand MESA #############################################################
+    # 1st flight Snow depth
+    f = join(reprojected, "ASO_GrandMesa_Mosaic_2020Feb1-2_snowdepth_50m.tif")
+    uploaders.append(UploadRasterBatch([f], date=date(2020, 2, 2), type="depth", units="meters", **kwargs))
 
-    # Reproject to the correct epsg
-    final = reproject(filenames, epsg, out_dir, adjust_vdatum=True)
+    # 1st flight SWE
+    f = join(reprojected, "ASO_GrandMesa_Mosaic_2020Feb1-2_swe_50m.tif")
+    uploaders.append(UploadRasterBatch([f], date=date(2020, 2, 2), type="swe", units="meters", **kwargs))
 
-    # 3, Pass them to you batch uploader you need
-    u = UploadRasterBatch(final, **kwargs)
+    # 2nd flight snow depth
+    f =  join(reprojected, "ASO_GrandMesa_Mosaic_2020Feb13_snowdepth_50m.tif")
+    uploaders.append(UploadRasterBatch([f], date=date(2020, 2, 13), type="depth", units="meters", **kwargs))
 
-    # 4. Push to the database and collect the errors from push function
-    u.push()
+    # 2nd flight snow depth
+    f = join(reprojected, "ASO_GrandMesa_Mosaic_2020Feb13_swe_50m.tif")
+    uploaders.append(UploadRasterBatch([f], date=date(2020, 2, 13), type="swe", units="meters", **kwargs))
 
-    return len(u.errors)
+    ########################################### East River #############################################################
 
+    errors = 0
+    for u in uploaders:
+        u.push()
+        errors += len(u.errors)
 
 # Add this so you can run your script directly without running run.py
 if __name__ == '__main__':
