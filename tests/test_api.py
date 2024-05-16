2d02fe73b5c9298a335ffd7aee363534c3e06e67
Micah Sandusky <micc5725@gmail.com>
2024-05-16 11:30:22 -0600

    start layer tests, not passing yet

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,7 +4,9 @@ import numpy as np
 import pytest
 from datetime import date
 
-from snowexsql.api import PointMeasurements, LargeQueryCheckException
+from snowexsql.api import (
+    PointMeasurements, LargeQueryCheckException, LayerMeasurements
+)
 from snowexsql.db import get_db, initialize
 
 
@@ -23,7 +25,13 @@ def db_url():
     return 'localhost/test'
 
 
-class TestPointMeasurements:
+class DBConnection:
+    """
+    Base class for connecting to the test database and overwiting the URL
+    so that we stay connected to our local testing DB
+    """
+    CLZ = PointMeasurements
+
     @pytest.fixture(scope="class")
     def db(self, creds, db_url):
         engine, session, metadata = get_db(
@@ -42,9 +50,17 @@ class TestPointMeasurements:
         """
         Extend the class and overwrite the database name
         """
-        class Extended(PointMeasurements):
+        class Extended(self.CLZ):
             DB_NAME = db_url
-        return Extended
+
+        yield Extended
+
+
+class TestPointMeasurements(DBConnection):
+    """
+    Test the Point Measurement class
+    """
+    CLZ = PointMeasurements
 
     def test_all_types(self, clz):
         result = clz().all_types
@@ -77,6 +93,98 @@ class TestPointMeasurements:
             ('mesa',),
             ('magnaprobe',),
             ('camera',)
+        ]
+
+    @pytest.mark.parametrize(
+        "kwargs, expected_length, mean_value", [
+            ({
+                "date": date(2020, 5, 28),
+                "instrument": 'camera'
+            }, 47, 2.194877),
+            ({"instrument": "magnaprobe", "limit": 10}, 10, 82.9),  # limit works
+            ({
+                 "date": date(2020, 5, 28),
+                 "instrument": 'pit ruler'
+             }, 0, np.nan)
+        ]
+    )
+    def test_from_filter(self, clz, kwargs, expected_length, mean_value):
+        result = clz.from_filter(**kwargs)
+        assert len(result) == expected_length
+        if expected_length > 0:
+            assert pytest.approx(result["value"].mean()) == mean_value
+
+    @pytest.mark.parametrize(
+        "kwargs, expected_error", [
+            ({"notakey": "value"}, ValueError),
+            ({"instrument": "magnaprobe"}, LargeQueryCheckException),
+            ({"date": [date(2020, 5, 28), date(2019, 10, 3)]}, ValueError),
+        ]
+    )
+    def test_from_filter_fails(self, clz, kwargs, expected_error):
+        """
+        Test failure on not-allowed key and too many returns
+        """
+        with pytest.raises(expected_error):
+            clz.from_filter(**kwargs)
+
+    def test_from_area(self, clz):
+        shp = gpd.points_from_xy(
+            [743766.4794971556], [4321444.154620216], crs="epsg:26912"
+        ).buffer(10)[0]
+        result = clz.from_area(
+            shp=shp,
+            date=date(2019, 10, 30)
+        )
+        assert len(result) == 2
+        assert all(result["value"] == 4.50196)
+
+    def test_from_area_point(self, clz):
+        pts = gpd.points_from_xy([743766.4794971556], [4321444.154620216])
+        crs = "26912"
+        result = clz.from_area(
+            pt=pts[0], buffer=10, crs=crs,
+            date=date(2019, 10, 30)
+        )
+        assert len(result) == 2
+        assert all(result["value"] == 4.50196)
+
+
+class TestLayerMeasurements(DBConnection):
+    """
+    Test the Layer Measurement class
+    """
+    CLZ = LayerMeasurements
+
+    def test_all_types(self, clz):
+        result = clz().all_types
+        assert result == [
+            ('sample_signal',), ('force',), ('density',), ('grain_size',),
+            ('reflectance',), ('permittivity',), ('lwc_vol',),
+            ('manual_wetness',), ('equivalent_diameter',),
+            ('specific_surface_area',), ('grain_type',), ('temperature',),
+            ('hand_hardness',)
+        ]
+
+    def test_all_dates(self, clz):
+        result = clz().all_dates
+        assert len(result) == 76
+        assert result[0] == (date(2020, 3, 12),)
+        assert result[-1] == (date(2020, 1, 29),)
+
+    def test_all_observers(self, clz):
+        result = clz().all_observers
+        assert result == [
+            (None,), ('Juha Lemmetyinen',), ('Kate Hale',), ('Céline Vargel',),
+            ('Carrie Vuyovich',), ('Juha Lemmetyinen & Ioanna Merkouriadi',),
+            ('Carrie Vuyovich & Juha Lemmetyinen',),
+            ('Kehan Yang',)] != [('Catherine Breen, Cassie Lumbrazo',),
+            (None,), ('Ryan Webb',), ('Randall Bonnell',), ('Tate Meehan',)
+        ]
+
+    def test_all_instruments(self, clz):
+        result = clz().all_instruments
+        assert result == [
         ]
 
     @pytest.mark.parametrize(
