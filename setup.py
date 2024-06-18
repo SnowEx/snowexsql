f42c0e09ad38e0dba8a63b3d42b1cd903371d51f
Micah Sandusky <micc5725@gmail.com>
2024-06-18 10:14:13 -0600

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SnowEx/snowexsql',
-    version='0.3.1',
+    version='0.4.0',
     zip_safe=False,
 )
--- snowexsql/__init__.py
+++ snowexsql/__init__.py
@@ -1,4 +1,4 @@
 """Top-level package for snowexsql."""
 
 __author__ = """Micah Johnson"""
-__version__ = '0.3.1'
+__version__ = '0.4.0'
