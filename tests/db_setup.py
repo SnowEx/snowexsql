2cca648aa430907335445cb03542074613cb5597
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-20 11:30:08 -0600

    Tests - Move more test DB information to the credentials file
    Even more flexibility when setting up tests locally

--- tests/db_setup.py
+++ tests/db_setup.py
@@ -1,3 +1,4 @@
+import json
 from os.path import dirname, join
 
 from snowexsql.db import get_db, initialize
@@ -8,18 +9,23 @@ class DBSetup:
     Base class for all our tests. Ensures that we clean up after every class
     that's run
     """
+    CREDENTIAL_FILE = join(dirname(__file__), 'credentials.json')
+    DB_INFO = json.load(open(CREDENTIAL_FILE))
+
+    @classmethod
+    def database_name(cls):
+        return f"{cls.DB_INFO["address"]}/{cls.DB_INFO["db_name"]}"
 
     @classmethod
     def setup_class(cls):
         """
         Setup the database one time for testing
         """
-        cls.db = 'localhost/test'
         cls.data_dir = join(dirname(__file__), 'data')
-        creds = join(dirname(__file__), 'credentials.json')
-
         cls.engine, cls.session, cls.metadata = get_db(
-            cls.db, credentials=creds, return_metadata=True
+            cls.database_name(),
+            credentials=cls.CREDENTIAL_FILE,
+            return_metadata=True
         )
 
         initialize(cls.engine)
