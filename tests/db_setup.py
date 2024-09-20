18859480739cc171b1fd75236b59434b42f14f9b
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-20 11:02:18 -0600

    Tests - Rename variable to indicate class method.
    Follows the Python convention.

--- tests/db_setup.py
+++ tests/db_setup.py
@@ -5,28 +5,31 @@ from snowexsql.db import get_db, initialize
 
 class DBSetup:
     """
-    Base class for all our tests. Ensures that we clean up after every class that's run
+    Base class for all our tests. Ensures that we clean up after every class
+    that's run
     """
 
     @classmethod
-    def setup_class(self):
+    def setup_class(cls):
         """
         Setup the database one time for testing
         """
-        self.db = 'localhost/test'
-        self.data_dir = join(dirname(__file__), 'data')
+        cls.db = 'localhost/test'
+        cls.data_dir = join(dirname(__file__), 'data')
         creds = join(dirname(__file__), 'credentials.json')
 
-        self.engine, self.session, self.metadata = get_db(self.db, credentials=creds, return_metadata=True)
+        cls.engine, cls.session, cls.metadata = get_db(
+            cls.db, credentials=creds, return_metadata=True
+        )
 
-        initialize(self.engine)
+        initialize(cls.engine)
 
     @classmethod
-    def teardown_class(self):
+    def teardown_class(cls):
         """
         Close the session
         """
-        self.session.close()  # optional, depends on use case
+        cls.session.close()  # optional, depends on use case
 
     def teardown(self):
         self.session.flush()
