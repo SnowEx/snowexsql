c9c3e453119e9750dbbf850f6aa4351816552aa4
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-03 16:09:49 -0600

    Tables - Remove outdated header docstring

--- snowexsql/tables/base.py
+++ snowexsql/tables/base.py
@@ -1,10 +1,3 @@
-"""
-Module contains all the data models for the database. Classes here actually
-represent tables where columns are mapped as attributed. Any class inheriting
-from Base is a real table in the database. This is called Object Relational
-Mapping in the sqlalchemy or ORM.
-"""
-
 from sqlalchemy import Column, Date, Integer
 from sqlalchemy.orm import DeclarativeBase
 
