a144963b5df61bb15b5a72379727acbbf9c2b749
Micah Sandusky <micc5725@gmail.com>
2024-10-03 11:39:25 -0600

    Consolodate layer location and observers to the sites table. Consolodate site conditions to the sites table

--- snowexsql/tables/__init__.py
+++ snowexsql/tables/__init__.py
@@ -1,7 +1,6 @@
 from .image_data import ImageData
 from .layer_data import LayerData
 from .point_data import PointData
-from .site_data import SiteCondition
 from .observers import Observer
 from .instrument import Instrument
 from .campaign import Campaign
--- snowexsql/tables/site.py
+++ snowexsql/tables/site.py
@@ -5,22 +5,57 @@ Created on Thu Aug 22 11:56:34 2024
 @author: jtmaz
 """
 
-from sqlalchemy import Column, String, Date
+from sqlalchemy import Column, String, Date, Float, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+from typing import List
 
-from .base import Base
+from .observers import Observer
+from .base import Base, SingleLocationData
 from .campaign import InCampaign
 from .doi import HasDOI
 
 
-class Site(Base, InCampaign, HasDOI):
+class SiteObservers(Base):
+    """
+    Link table
+    """
+    __tablename__ = 'site_observers'
+
+    site_id = Column(Integer, ForeignKey('public.sites.id'))
+    observer_id = Column(Integer, ForeignKey("public.observers.id"))
+
+
+class Site(SingleLocationData, Base, InCampaign, HasDOI):
     """
     Table stores Site data. Does not store data values,
     it only stores the site metadata.
     """
-    # TODO: add geometry here and remove from site_condtions
     __tablename__ = 'sites'
 
-    name = Column(String())
+    name = Column(String())  # This can be pit_id
     description = Column(String())
     # Date of the measurement
     date = Column(Date)
+
+    # Link the observer
+    # id is a mapped column for many-to-many with observers
+    id: Mapped[int] = mapped_column(primary_key=True)
+    observers: Mapped[List[Observer]] = relationship(
+        secondary=SiteObservers.__table__
+    )
+
+    # Conditions for this site and date
+    slope_angle = Column(Float)
+    aspect = Column(Float)
+    air_temp = Column(Float)
+    total_depth = Column(Float)
+    weather_description = Column(String())
+    precip = Column(String())
+    sky_cover = Column(String())
+    wind = Column(String())
+    ground_condition = Column(String())
+    ground_roughness = Column(String())
+    ground_vegetation = Column(String())
+    vegetation_height = Column(String())
+    tree_canopy = Column(String())
+    site_notes = Column(String())
