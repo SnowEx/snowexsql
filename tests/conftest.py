a930a5666f75a9eb6ce5d6094e76b138548eee58
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-07 12:46:25 -0600

    Tests - Setup fixtures for db session
    Add global pytest fixtures for the db session that allows for rolling
    back any DB changes after a test has run. This allows for keeping the
    schema during a whole test suite run, while changing specific test
    run data setups.

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+from pytest_factoryboy import register
+from sqlalchemy import create_engine
+
+from snowexsql.db import db_connection_string, initialize
+from tests.factories import (
+    CampaignFactory, DOIFactory, InstrumentFactory, MeasurementTypeFactory,
+    ObserverFactory, PointDataFactory, PointObservationFactory
+)
+from .db_setup import CREDENTIAL_FILE, DB_INFO, SESSION
+
+# Make factories available to tests
+register(CampaignFactory)
+register(DOIFactory)
+register(InstrumentFactory)
+register(MeasurementTypeFactory)
+register(ObserverFactory)
+register(PointDataFactory)
+register(PointObservationFactory)
+
+
+@pytest.fixture(scope="session")
+def connection():
+    database_name = DB_INFO["address"] + "/" + DB_INFO["db_name"]
+    db_string = db_connection_string(database_name, CREDENTIAL_FILE)
+
+    engine = create_engine(db_string)
+    initialize(engine)
+    connection = engine.connect()
+
+    yield connection
+
+    connection.close()
+    engine.dispose()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def db_session(connection):
+    # Based on:
+    # https://docs.sqlalchemy.org/en/20/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
+
+    transaction = connection.begin()
+
+    # Configure session
+    SESSION.configure(
+        bind=connection, join_transaction_mode="create_savepoint"
+    )
+
+    # Create a new session
+    session = SESSION()
+
+    yield session
+
+    # rollback
+    # Everything that happened with the Session above
+    # (including calls to commit()) are rolled back.
+    session.close()
+    transaction.rollback()
