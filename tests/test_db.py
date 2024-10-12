195488177975bdde113d4858be81185b79420fd1
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-12 12:31:51 -0600

    Tests - DB - Break up test case class
    Separate the test file in one class that tests DB tables and the other
    the db module of the library.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,11 @@
 import pytest
-from sqlalchemy import Engine, MetaData, Table
+from sqlalchemy import Engine, MetaData
 from sqlalchemy.orm import Session
 
 import snowexsql
 from snowexsql.db import (
-    DB_CONNECTION_PROTOCOL, db_connection_string, get_db, get_table_attributes,
-    load_credentials
+    DB_CONNECTION_PROTOCOL, db_connection_string, get_db, load_credentials
 )
-from snowexsql.tables import LayerData, Site
 from .db_setup import DBSetup, DB_INFO
 
 
@@ -21,58 +19,6 @@ def db_connection_string_patch(monkeypatch, test_db_info):
         'db_connection_string',
         db_string
     )
-
-
-class TestDB(DBSetup):
-    single_loc_atts = ['elevation', 'geom', 'time']
-
-    meas_atts = ['measurement_type_id']
-
-    site_atts = single_loc_atts + \
-                ['slope_angle', 'aspect', 'air_temp', 'total_depth',
-                 'weather_description', 'precip', 'sky_cover', 'wind',
-                 'ground_condition', 'ground_roughness',
-                 'ground_vegetation', 'vegetation_height',
-                 'tree_canopy', 'site_notes']
-
-    layer_atts = meas_atts + \
-                 ['depth', 'value', 'bottom_depth', 'comments', 'sample_a',
-                  'sample_b', 'sample_c']
-    raster_atts = meas_atts + ['raster', 'description']
-
-    def setup_class(self):
-        """
-        Setup the database one time for testing
-        """
-        super().setup_class()
-        # only reflect the tables we will use
-        self.metadata.reflect(self.engine, only=['layers'])
-
-    def test_layer_structure(self):
-        """
-        Tests our tables are in the database
-        """
-        t = Table("layers", self.metadata, autoload=True)
-        columns = [m.key for m in t.columns]
-
-        for c in self.layer_atts:
-            assert c in columns
-
-    @pytest.mark.parametrize(
-        "DataCls,attributes",
-        [
-            (Site, site_atts),
-            (LayerData, layer_atts),
-        ]
-    )
-    def test_get_table_attributes(self, DataCls, attributes):
-        """
-        Test we return a correct list of table columns from db.py
-        """
-        atts = get_table_attributes(DataCls)
-
-        for c in attributes:
-            assert c in atts
 
 
 class TestDBConnectionInfo:
