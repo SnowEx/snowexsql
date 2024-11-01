6c4094c80a218d3d22cd2f165355b9dc712555e5
aaarendt <arendta@uw.edu>
2024-11-01 10:07:21 -0700

    add new layer density factory

--- tests/factories/layer_data.py
+++ tests/factories/layer_data.py
@@ -6,7 +6,9 @@ from .doi import DOIFactory
 from .instrument import InstrumentFactory
 from .measurement_type import MeasurementTypeFactory
 from .site import SiteFactory
-
+from geoalchemy2.elements import WKTElement
+from datetime import datetime
+from .campaign import CampaignFactory
 
 class LayerDataFactory(BaseFactory):
     class Meta:
@@ -23,3 +25,19 @@ class LayerDataFactory(BaseFactory):
     instrument = factory.SubFactory(InstrumentFactory, name='Density Cutter')
     doi = factory.SubFactory(DOIFactory)
     site = factory.SubFactory(SiteFactory)
+
+
+class LayerDensityFactory(LayerDataFactory):
+
+    depth = 15.0
+    bottom_depth = 5.0
+    value = '236.0'
+    comments = 'Sample_A'
+    
+    site = factory.SubFactory(
+        SiteFactory, 
+        name = 'IN20', 
+        datetime=datetime(2020, 2, 15, 13, 30),
+        geom = WKTElement("POINT(743281 4324005)", srid=32612),
+        campaign = factory.SubFactory(CampaignFactory, name='Grand Mesa')
+    )
