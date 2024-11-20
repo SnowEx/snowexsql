472a882c102dd20d513f551ecc2d1fff09a78253
aaarendt <arendta@uw.edu>
2024-11-20 14:10:10 -0800

    add tests from area

--- tests/api/test_layer_measurements.py
+++ tests/api/test_layer_measurements.py
@@ -2,14 +2,21 @@
 Test the Layer Measurement class
 """
 import pytest
-import datetime
+from datetime import date, timedelta
+import geopandas as gpd
 
 from geoalchemy2.shape import to_shape
-from geoalchemy2.elements import WKTElement
 
 from snowexsql.api import LayerMeasurements
 from snowexsql.tables import LayerData
 
+@pytest.fixture
+def point_data_x_y(layer_data_factory):
+    return to_shape(layer_data_factory.build().site.geom)
+
+@pytest.fixture
+def point_data_srid(layer_data_factory):
+    return layer_data_factory.build().site.geom.srid
 
 @pytest.fixture
 def layer_data(layer_data_factory, db_session):
@@ -54,15 +61,10 @@ class TestLayerMeasurements:
             for record in self.db_data
         ]
 
-@pytest.fixture
-def layer_data(layer_density_factory, db_session):
-    layer_density_factory.create()
-    return db_session.query(LayerData).all()
-
 @pytest.mark.usefixtures("db_test_session")
 @pytest.mark.usefixtures("db_test_connection")
 @pytest.mark.usefixtures("layer_data")
-class TestDensityLayerMeasurement:
+class TestLayerMeasurementFilter:
     @pytest.fixture(autouse=True)
     def setup_method(self, layer_data):
         self.subject = LayerMeasurements()
@@ -77,9 +79,9 @@ class TestDensityLayerMeasurement:
             assert len(result) == 1
             assert result.loc[0].value == self.db_data.value
 
-    def test_instrument_and_limit(self, layer_density_factory):
+    def test_instrument_and_limit(self, layer_data_factory):
         # Create 10 more records, but only fetch five
-        layer_density_factory.create_batch(10)
+        layer_data_factory.create_batch(10)
 
         result = self.subject.from_filter(
             instrument=self.db_data.instrument.name,
@@ -88,6 +90,19 @@ class TestDensityLayerMeasurement:
         assert len(result) == 5
         assert pytest.approx(result["value"].astype("float").mean()) == \
         float(self.db_data.value)
+
+    def test_no_instrument_on_date(self):
+        result = self.subject.from_filter(
+            date=self.db_data.site.datetime.date() + timedelta(days=1),
+            instrument=self.db_data.instrument.name,
+        )
+        assert len(result) == 0
+
+    def test_unknown_instrument(self):
+        result = self.subject.from_filter(
+            instrument='Does not exist',
+        )
+        assert len(result) == 0
 
     def test_date_and_measurement_type(self):
         result = self.subject.from_filter(
@@ -103,3 +118,35 @@ class TestDensityLayerMeasurement:
         )
         assert len(result) == 1
         assert result.loc[0].value == self.db_data.value
+
+
+    @pytest.mark.parametrize(
+        "kwargs, expected_error", [
+            ({"notakey": "value"}, ValueError),
+            ({"date": [date(2020, 5, 28), date(2019, 10, 3)]}, ValueError),
+        ]
+    )
+    def test_from_filter_fails(self, kwargs, expected_error):
+        """
+        Test failure on not-allowed key and too many returns
+        """
+        with pytest.raises(expected_error):
+            self.subject.from_filter(**kwargs)
+
+    def test_from_area(self,point_data_x_y, point_data_srid):
+        shp = gpd.points_from_xy(
+            [point_data_x_y.x],
+            [point_data_x_y.y],
+            crs=f"epsg:{point_data_srid}"
+        ).buffer(10)[0]
+        result = self.subject.from_area(shp=shp)
+        assert len(result) == 1
+
+    def test_from_area_point(self, point_data_x_y, point_data_srid):
+        pts = gpd.points_from_xy(
+            [point_data_x_y.x],
+            [point_data_x_y.y],
+        )
+        crs = point_data_srid
+        result = self.subject.from_area(pt=pts[0], buffer=10, crs=crs)
+        assert len(result) == 1
